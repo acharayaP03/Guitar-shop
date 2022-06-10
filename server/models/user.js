53f84cf9f4857b2b50f8f30203bf60dd5a72cb9e
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-06-10 21:31:59 +1000

    user model and schema'

--- server/models/user.js
+++ server/models/user.js
@@ -4,14 +4,52 @@ const validator = require('validator');
 require('dotenv').config()
 
 const userSchema = new mongoose.Schema({
-    email:{},
-    password: {},
-    role: {},
-    firstname: {},
-    lastname: {},
-    cart: {},
-    history: {},
-    varified: {}
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email')
+            }
+        } 
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    firstname: {
+        type: String,
+        maxlength: 100,
+        trim:true,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        maxlength: 100,
+        trim:true,
+        default: ''
+    },
+    cart: {
+        type: Array,
+        default: []
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+    varified: {
+        type: Boolean,
+        default: false
+    }
 })
 
-module.exports = userSchema;
+const User = mongoose.model('User', userSchema)
+module.exports = { User };
