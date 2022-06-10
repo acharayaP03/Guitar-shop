f668a7dee3e66f5627aa3863c7e2fd82d16eb60c
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-06-10 13:52:59 +1000

    server script setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
-const express = require('express');
 
+const express = require('express');
+const mongoose = require('mongoose');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 const app = express();
 
+/**
+ * @middlewares
+ */
+app.use(xss()) // prevents script from executing in the server.
+app.use(mongoSanitize());
+
+/**
+ * @Port
+ */
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () =>{
+    // tslint:disable-next-line:no-console
     console.log(`Server is listening on port ${PORT}`)
 });
