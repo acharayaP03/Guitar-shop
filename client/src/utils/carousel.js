6e6cebe5711c109da44f0993e09c1891d096c753
Trishten ac <trishtenac@trishtens-mbp.lan>
2022-08-18 19:51:46 +1000

    resuable button

--- client/src/utils/carousel.js
+++ client/src/utils/carousel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
-
+import {WavesButton} from "./tools";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -22,15 +22,21 @@ const Carrousel = ({items}) => {
                 <div key={i}>
                     <div className="featured_image"
                          style={{
-                             background:`url(${item.img})`,
-                             height:`${window.innerHeight}px`,
+                             background:`url(${item.img})`
                          }}
                     >
                         <div className="featured_action">
                             <div className="tag title">{item.lineOne}</div>
                             <div className="tag low_title">{item.lineTwo}</div>
                             <div>
-                                BUTTON
+                                <WavesButton
+                                    type="default"
+                                    title={item.linkTitle}
+                                    linkTo={item.linkTo}
+                                    style={{
+                                        margin:'10px 0 0 0'
+                                    }}
+                                />
                             </div>
                         </div>
                     </div>
