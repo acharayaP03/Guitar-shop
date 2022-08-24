3ac62149199bbed1e7bc31583bde916d7840bf06
acharayaP03 <acharyaP03@gmail.com>
2022-08-25 08:51:22 +1000

    bug in route guard

---
+++ client/src/hoc/auth.guard.js
@@ -0,0 +1,44 @@
+import React,{ useState, useEffect} from 'react';
+import { useSelector } from 'react-redux'
+import {Loader} from 'utils/tools';
+import { useNavigate} from "react-router-dom";
+import {replace} from "formik";
+
+/**
+ *
+ * @param ComposedComponent wrop the component that needs to be guarded
+ * @returns {(function(*): *)|*}
+ * checks if the user is logged in to access the dashboard.
+ */
+
+export default function authGuard(ComposedComponent){
+    const AuthenticationCheck = (props) => {
+        const [isAuth,setIsAuth] = useState(false);
+        const users  = useSelector( state => state.users );
+        const navigation = useNavigate()
+
+        useEffect(()=>{
+            if(!users.auth){
+                navigation('/', { replace: true})
+            }else{
+                setIsAuth(true);
+            }
+        },[props,users]);
+
+
+        if(!isAuth){
+            return(
+                <Loader full={true}/>
+            )
+        } else {
+            return(
+                <ComposedComponent users={users} {...props}/>
+            )
+        }
+
+
+
+
+    }
+    return AuthenticationCheck;
+}
