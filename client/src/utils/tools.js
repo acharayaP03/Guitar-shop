0ee56bf47dcffe90a56197aac8a12d6ec1be344d
acharayaP03 <acharyaP03@gmail.com>
2022-09-20 20:15:35 +1000

    image uploader component

--- client/src/utils/tools.js
+++ client/src/utils/tools.js
@@ -43,7 +43,7 @@ export const renderCardImage = (image) => {
     if(image.length > 0){
         return image[0]
     }else{
-        return '/images/image_not_availble.png'
+        return '/images/img.png'
     }
 }
 /**
