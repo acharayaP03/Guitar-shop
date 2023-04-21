f038b1b798bfc4cb8d327c4dcaf91204982411aa
Prabhakar Acharya <42729832+acharayaP03@users.noreply.github.com>
2023-04-21 12:26:13 +1000

    didrectly opened from vs code

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import ReduxStore from "./store";
 import 'resources/styles/styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/**
+ * opened from vs code on web 
+ */
 root.render(
     <Provider store={ReduxStore()}>
         <React.StrictMode>
