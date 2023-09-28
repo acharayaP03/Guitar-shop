7a680408538c4c219e9d20ee05c399084077a2c7
acharayaP03 <acharyaP03@gmail.com>
2023-09-28 23:05:44 +1000

    inittial lay out for shop

--- client/src/components/shop/index.js
+++ client/src/components/shop/index.js
@@ -6,7 +6,7 @@ import { getAllBrands } from 'store/actions/brands.actions'
 
 // eslint-disable-next-line no-unused-vars
 import GridOff from '@material-ui/icons/GridOff'
-import { GridOn } from '@material-ui/icons'
+import GridOn from '@material-ui/icons/GridOn'
 
 const Shop = () => {
     const defaultValues = {
@@ -22,9 +22,12 @@ const Shop = () => {
         (state, newState) => ({ ...state, ...newState }),
         defaultValues
     )
+
+    const { byPaginate } = useSelector((state) => state.products)
     const brands = useSelector((state) => state.brands)
     const dispatch = useDispatch()
 
+    const handleGridAction = () => setGrid(!grid)
     useEffect(() => {
         dispatch(getAllBrands())
     }, [dispatch])
@@ -33,7 +36,55 @@ const Shop = () => {
     useEffect(() => {
         dispatch(getProductByPaginate(searchValues))
     }, [searchValues, dispatch])
-    return <>Shop</>
+    return (
+        <div className="page_container">
+            <div className="page_top">
+                <div className="container">FORM</div>
+            </div>
+            <div className="container">
+                <div className="shop_wrapper">
+                    <div className="left">
+                        collapse brand collapse frets collapse price
+                    </div>
+                    <div className="right">
+                        <div className="shop_options">
+                            <div className="shop_grids clear">
+                                <div
+                                    className={`grid_btn ${
+                                        grid ? '' : 'active'
+                                    }`}
+                                    onClick={() => handleGridAction()}
+                                >
+                                    <GridOn />
+                                </div>
+                                <div
+                                    className={`grid_btn ${
+                                        !grid ? '' : 'active'
+                                    }`}
+                                    onClick={() => handleGridAction()}
+                                >
+                                    <GridOff />
+                                </div>
+                            </div>
+                            <div>
+                                {byPaginate && byPaginate.docs ? (
+                                    <> we have plenty of products to show</>
+                                ) : null}
+                            </div>
+                        </div>
+                        <div>
+                            <div className="shop_products">
+                                <div className="shop_grids clear products_row">
+                                    products
+                                </div>
+                            </div>
+                            <div className="shop_pagination">pagination</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Shop
