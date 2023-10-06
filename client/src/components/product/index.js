a7a5e21e01186fb3ebf48468cdb81bf34595a2c3
acharayaP03 <acharyaP03@gmail.com>
2023-10-06 21:48:28 +1100

    product carousel

--- client/src/components/product/index.js
+++ client/src/components/product/index.js
@@ -7,7 +7,28 @@ import { useParams } from 'react-router-dom'
 import { clearCurrentProduct } from 'store/actions'
 import ProductInfo from './detail'
 import { renderCardImage } from 'utils/tools'
+
+import { Modal } from 'react-bootstrap'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 const Product = (props) => {
+    const [modal, setModal] = useState(false)
+    const settings = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
+
+    const handleCloseModal = () => setModal(false)
+    const handleOpenModal = () => setModal(true)
+
     const { id } = useParams()
     const products = useSelector((state) => state.products)
     console.log('products', products)
@@ -38,7 +59,7 @@ const Product = (props) => {
                                         products.productById.images
                                     )}
                                     style={{ cursor: 'pointer' }}
-                                    onClick={() => alert('add to cart..')}
+                                    onClick={() => handleOpenModal()}
                                 />
                             </div>
                         </div>
@@ -50,6 +71,35 @@ const Product = (props) => {
                     <Loader />
                 )}
             </div>
+            <Modal
+                show={modal}
+                onHide={handleCloseModal}
+                dialogClassName="modal-90w"
+            >
+                <Modal.Header closeButton style={{ background: 'white' }}>
+                    <Modal.Title>Product Images</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{ background: 'gray' }}>
+                    <Slider {...settings}>
+                        {products.productById && products.productById.images
+                            ? products.productById.images.map((item, i) => (
+                                  <div key={item} style={{ margin: '0 auto' }}>
+                                      <div className="img-block">
+                                          <img
+                                              src={item}
+                                              alt={item}
+                                              style={{
+                                                  objectFit: 'contain',
+                                                  width: 'auto',
+                                              }}
+                                          />
+                                      </div>
+                                  </div>
+                              ))
+                            : null}
+                    </Slider>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
